e21fb9055750b3969bc68d9d2ef558b333565f54
kokonattuDream <32606269+kokonattuDream@users.noreply.github.com>
2018-04-15 09:20:41 -0400

    Saving Transaction and primary transaction page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,13 +13,17 @@ import { LoginService } from './login.service';
 import { UserService } from './user.service';
 
 import { UserAccountComponent } from './user-account/user-account.component';
+import { PrimaryTransactionComponent } from './primary-transaction/primary-transaction.component';
+import { SavingsTransactionComponent } from './savings-transaction/savings-transaction.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     LoginComponent,
-    UserAccountComponent
+    UserAccountComponent,
+    PrimaryTransactionComponent,
+    SavingsTransactionComponent
   ],
   imports: [
     BrowserModule,
